94bc805cbaa91a7c7cc7f109d1b5a392d6ebc6e6
dongeunshin <7017410@naver.com>
2025-04-11 03:21:16 +0900

    feat: rageButton을 누르면 누른시간을 hitory버튼을 통해 확인 할 수 있는 기능 구현

---
+++ Phew/Phew/PhewApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhewApp.swift
+//  Phew
+//
+//  Created by dong eun shin on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PhewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Phew/PhewTests/PhewTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhewTests.swift
+//  PhewTests
+//
+//  Created by dong eun shin on 4/11/25.
+//
+
+import Testing
+@testable import Phew
+
+struct PhewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
