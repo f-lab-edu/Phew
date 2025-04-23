45c30c80ba208b77d07096540e348ae7e179900b
dongeunshin <7017410@naver.com>
2025-04-23 21:35:49 +0900

    [9] .gitignore 추가

---
+++ Phew/Phew/PhewApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhewApp.swift
+//  Phew
+//
+//  Created by dong eun shin on 4/23/25.
+//
+
+import SwiftUI
+
+@main
+struct PhewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
